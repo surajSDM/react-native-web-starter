ddf6c3b882179b6e6418abb61a8b2a04b0e6a48b
Suraj Sanwal <suraj.sanwal@smartdatainc.net>
2018-12-10 15:49:33 +0530

    "source uypdate"

--- index.web.js
+++ index.web.js
@@ -1,20 +1,6 @@
 import React from "react";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
-
-class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.box}>
-        <Text style={styles.text}>Hello, world!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  box: { padding: 10 },
-  text: { fontWeight: "bold" }
-});
+import { AppRegistry } from "react-native";
+import App from "./src/App";
 
 AppRegistry.registerComponent("App", () => App);
 AppRegistry.runApplication("App", {
